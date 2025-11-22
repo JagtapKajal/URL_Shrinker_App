26215ddfa706a04e661417545ea4c51197ca7723
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-22 16:37:20 +0530

    Write format for short and click as well and export short url schema

--- models/Shorturl.js
+++ models/Shorturl.js
@@ -5,4 +5,16 @@ const ShortUrlSchema = new mongoose.Schema({
         type: String,
         required :true
     },
+     short :{
+        type : String,
+        required : true,
+        default : shortid.generate
+    },
+    clicks : {
+        type : Number,
+        required : true,
+        default : 0
+    }
 })
+
+module.exports = mongoose.model('shorturl', ShortUrlSchema)
