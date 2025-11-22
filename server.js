04e661d2d44ea1e3d3f0cc94fd3f53d174c867f1
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-22 16:43:03 +0530

    Also write for getting url from shorturl class

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const Shorturl = require('./models/Shorturl')
+
 
 const app = express()
 
@@ -6,3 +8,8 @@ mongoose.connect('mongodb://localhost/urlshortener');
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
+
+app.get('/',async (req, res) =>{
+  const shorturls = await Shorturl.find()
+res.render('index', {shorturls: shorturls})
+})
