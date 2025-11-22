89cdb4238bcf785b6f2afc6466c25943ff9fe55f
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-22 16:46:04 +0530

    Write a condition while getting url if we get it then ok but if not then it will show 404 status

--- server.js
+++ server.js
@@ -20,3 +20,16 @@ Shorturl.create({full:req.body.Fullurl})
 
 res.redirect('/')
 })
+
+app.get('/:Shorturl', async(req, res) =>{
+   const shorturl = await Shorturl.findOne({short: req.params.Shorturl}) 
+   if(shorturl == null) return res.sendStatus(404)
+
+    shorturl.clicks++
+    shorturl.save()
+
+    res.redirect(shorturl.full)
+})
+
+
+app.listen(process.env.PORT || 5000);
